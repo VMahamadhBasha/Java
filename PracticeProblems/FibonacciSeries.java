6e26e835d98d87016bccd28b732dc1085a7b0420
VMahamadhBasha <vmahamadhbasha@gmail.com>
2026-07-18 19:52:07 +0530

    Add result accumulator for Fibonacci series
    Added a variable to accumulate the Fibonacci series result.

--- PracticeProblems/FibonacciSeries.java
+++ PracticeProblems/FibonacciSeries.java
@@ -6,9 +6,11 @@ public class Main
 		int n1=0;
         int n2=1;
         int t=0;
+		int res=0;
         System.err.print(n1+" "+n2+" ");
         for(int i=0;i<10;i++){
             t=n1+n2;
+			res+=t;
             n1=n2;
             n2=t;
 
